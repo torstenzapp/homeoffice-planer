702d323635466e880d6229e6cf9626f5f111d488
torstenzapp <79056555+torstenzapp@users.noreply.github.com>
2025-08-18 19:44:13 +0200

    app.js

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ import { getDatabase, ref, set, update, remove, onValue, off } from "https://www
 
 // Deine Firebase-Konfiguration wurde hier eingefügt
 const firebaseConfig = {
-    apiKey: "AIzaSyDtPxYtqFSUcWrT7zJ-mjAQyPvCsMYZ6zg",
-    authDomain: "ho-planerv2.firebaseapp.com",
-    databaseURL: "https://ho-planerv2-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "ho-planerv2",
-    storageBucket: "ho-planerv2.firebasestorage.app",
-    messagingSenderId: "203584570304",
-    appId: "1:203584570304:web:4c5337ce60ed52ed67fe25",
-    measurementId: "G-1QB1ZM8CBL" // Measurement ID ist optional
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdLYCXvtuXPUhehE-QfqaXWRfseGfwzf4",
+  authDomain: "homeoffice-planer-drv.firebaseapp.com",
+  databaseURL: "https://homeoffice-planer-drv-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "homeoffice-planer-drv",
+  storageBucket: "homeoffice-planer-drv.firebasestorage.app",
+  messagingSenderId: "669565818222",
+  appId: "1:669565818222:web:9eb342704c1a74c5eedd7f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 
 class HomeOfficePlanner {
